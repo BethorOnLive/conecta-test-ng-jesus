3b0977d6a86dc4407d66cb0a743f59505c381c7e
Jesús <bethor18@gmail.com>
2022-11-24 23:55:52 -0600

    Maqueta bankapp1.jpg

--- bankapp/src/app/app.module.ts
+++ bankapp/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopSectionComponent } from './top-section/top-section.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MainTableComponent } from './main-table/main-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopSectionComponent,
+    SideBarComponent,
+    MainTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxDatatableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
